92e48f05a25c486a9a58a956c40cbd8f1531d63c
xwh123807 <xwh123807@gmail.com>
2017-09-10 22:25:58 +0800

    add weui

--- easycloud/pages/index/index.js
+++ easycloud/pages/index/index.js
@@ -15,6 +15,11 @@ Page({
       url: '../logs/logs'
     })
   },
+  bindFapiaoView: function() {
+    wx.navigateTo({
+      url: '../fapiao/list'
+    })
+  },
   onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
